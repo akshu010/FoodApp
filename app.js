5bd9d8ee474839482ea426422f014a42a69d7097
Akshay <akshusharmagi@gmail.com>
2024-09-29 18:57:03 +0530

    Episode 3 React component

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+const Elem =()=> (<p> Hello Everyone I am a p </p>)
 
+const HeadingComponent = () => (
+  <div id="container">
+    <h1>Namaste React by functional component</h1>
+  </div>
+);
+const title = (<h1>
+  {Elem()}
+  <Elem/>
+  <Elem></Elem>
+  Namaste React by first component
+  < HeadingComponent/>
+  </h1>)
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
+root.render(title);
